ce2dc09ccee55f82207743eda21bd1936c841125
lazaroconceicao <joselazaroc@gmail.com>
2023-01-31 22:55:21 -0300

    Finalizing functionality

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,16 +1,17 @@
 import Header from 'components/Header';
 import Item from 'components/Item';
-// import Item from 'components/Item';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import styles from './Categoria.module.scss';
 
 export default function Categoria() {
-  const { nomeCategoria } = useParams();
-  const { categoria, itens } = useSelector(state => ({
-    categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
-    itens: state.itens.filter(item => item.categoria === nomeCategoria),
-  }));
+  const { nomeCategoria } = useParams();  
+  const { categoria, itens } = useSelector(state => {
+    const regexp = new RegExp(state.busca, 'i')
+    return {categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
+    itens: state.itens.filter(item => item.categoria === nomeCategoria && item.titulo.match(regexp)),
+    }
+  });
   return (
     <div>
       <Header
